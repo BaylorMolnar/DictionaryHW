c31744fa90dd9600c2ac92466ee5a8a64f4d91ee
Baylor Molnar <baylor_molnar1@baylor.edu>
2021-02-03 23:58:09 -0600

    initial finish

--- World_Series.py
+++ World_Series.py
@@ -1,9 +1,22 @@
 def main():
 
-    trying_2()
+    year = input("Enter a year between 1903 and 2008(excluding 1904 and 1994): ")
+
+    team = years_and_teams(year)
+    amount = create_count(team)
+
+    print(
+        "In",
+        year,
+        "the winner of the World Series was",
+        team,
+        "who have won the World Series",
+        amount,
+        "times.",
+    )
 
 
-def create_count():
+def create_count(team_name):
     text_file = open("WorldSeriesWinners.txt", "r")
 
     TeamWins = {}
@@ -15,27 +28,33 @@ def create_count():
                 TeamWins[word] += 1
             else:
                 TeamWins[word] = 1
-    print(TeamWins)
-    for TextWord in TeamWins.keys():
-        print(TextWord.rstrip(" "), ":", TeamWins[TextWord])
 
+    # for TextWord in TeamWins.keys():
+    # print(TextWord.rstrip(" "), ":", TeamWins[TextWord])
+
+    value = TeamWins.get(team_name)
+
+    return value
     text_file.close()
 
 
-def trying_2():
+def years_and_teams(year_):
     same_file = open("WorldSeriesWinners.txt", "r")
     team_year = {}
     first_year = 1903
-    team_year[first_year] = "no"
     for line in same_file:
         if first_year < 2009:
-            team_year[first_year] = line.strip()
+            team_year[str(first_year)] = line.strip()
             if first_year == 1903 or first_year == 1993:
                 first_year += 2
             else:
                 first_year += 1
 
-    print(team_year)
+    value = team_year.get(str(year_))
+
+    return value
+
+    same_file.close()
 
 
 main()
